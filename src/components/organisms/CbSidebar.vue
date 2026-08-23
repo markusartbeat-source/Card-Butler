bb5d7e65ac3a0f009513e3137ecb119598fb67d4
Markus Weinert <markusartbeat@gmail.com>
2026-08-23 20:15:59 +0200

    Logo

--- src/components/organisms/CbSidebar.vue
+++ src/components/organisms/CbSidebar.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="h-screen w-64 shrink-0 p-3">
-    <div class="bg-surface h-full w-full rounded-3xl shadow-lg"></div>
+    <div class="bg-surface flex h-full w-full flex-col items-center rounded-3xl p-3 shadow-lg">
+      <img src="../../assets/logos/logo.png" alt="Logo" class="w-10 -rotate-1 drop-shadow-lg" />
+    </div>
   </div>
 </template>
