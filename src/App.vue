608c858332f7321fe4b91f9defee8a57d048dfb0
Markus Weinert <markusartbeat@gmail.com>
2026-08-23 14:42:56 +0200

    Hintergrund + Sidebarkontainer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <main>
-    <span class="material-symbols-outlined">add</span>
-  </main>
+  <div class="bg-background flex min-h-screen">
+    <CbSidebar />
+    <main>
+      <span class="material-symbols-outlined">add</span>
+    </main>
+  </div>
 </template>
+
+<script setup lang="ts">
+import CbSidebar from './components/organisms/CbSidebar.vue'
+</script>
---
+++ src/components/organisms/CbSidebar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="h-screen w-64 shrink-0 p-3">
+    <div class="bg-surface h-full w-full rounded-3xl shadow-lg"></div>
+  </div>
+</template>
