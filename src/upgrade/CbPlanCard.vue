8755bd951a08aa1f53f2946b70a40b1a90b356bb
Markus Weinert <markusartbeat@gmail.com>
2026-08-27 17:44:59 +0200

    Upgrade Buttons auswählen

--- src/upgrade/CbPlanCard.vue
+++ src/upgrade/CbPlanCard.vue
@@ -5,6 +5,17 @@
     <p class="mt-1 text-sm text-white/70">{{ subtitle }}</p>
     <p class="mt-6 text-4xl font-bold">{{ price }}€</p>
 
+    <!-- Only the paid plans can be bought. -->
+    <template v-if="price > 0">
+      <CbButton class="mt-6">
+        <CbIcon name="arrow_circle_up" />
+        Upgrade
+      </CbButton>
+      <p class="mt-2 text-center text-xs text-white/70">
+        Keine Verpflichtungen | Jederzeit kündbar
+      </p>
+    </template>
+
     <CbDivider class="my-6" />
 
     <p class="mb-4 text-sm text-white/70">{{ benefitsTitle }}</p>
@@ -15,8 +26,10 @@
 </template>
 
 <script setup lang="ts">
+import CbButton from '../components/atoms/CbButton.vue'
 import CbCheckLabel from '../components/atoms/CbCheckLabel.vue'
 import CbDivider from '../components/atoms/CbDivider.vue'
+import CbIcon from '../components/atoms/CbIcon.vue'
 
 defineProps<{
   name: string
