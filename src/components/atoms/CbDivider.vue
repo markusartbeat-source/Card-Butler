d5b312fb800bc490f68b7aea146560a8b897469f
Markus Weinert <markusartbeat@gmail.com>
2026-08-27 17:34:08 +0200

    Neuer devider

---
+++ src/components/atoms/CbDivider.vue
@@ -0,0 +1,4 @@
+<template>
+  <!-- A thin horizontal line to separate sections from each other. -->
+  <hr class="border-0 border-t border-surface-light" />
+</template>
--- src/pages/UpgradePage.vue
+++ src/pages/UpgradePage.vue
@@ -4,11 +4,15 @@
   <div class="relative z-10">
     <h1 class="p-6 text-3xl text-white">Pläne, die mit dir wachsen</h1>
 
+    <!-- Temporary preview of the new divider, removed again in a later step. -->
+    <CbDivider class="mx-6" />
+
     <!-- The plan cards sit centered in the space below the heading. -->
     <div class="mt-16 flex justify-center gap-8"></div>
   </div>
 </template>
 
 <script setup lang="ts">
+import CbDivider from '../components/atoms/CbDivider.vue'
 import CbGoldGlow from '../upgrade/CbGoldGlow.vue'
 </script>
