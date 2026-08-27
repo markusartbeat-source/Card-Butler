3b3d683d0dd5ea1033fe79d0889ed3bd066e4e7d
Markus Weinert <markusartbeat@gmail.com>
2026-08-27 17:16:36 +0200

    Überschrift und zentrierung

--- src/pages/UpgradePage.vue
+++ src/pages/UpgradePage.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1 class="text-3xl text-white">Upgrade</h1>
+  <h1 class="p-6 text-3xl text-white">Pläne, die mit dir wachsen</h1>
+
+  <!-- The plan cards sit centered in the space below the heading. -->
+  <div class="mt-16 flex justify-center gap-8"></div>
 </template>
