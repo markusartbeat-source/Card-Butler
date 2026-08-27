030fa5f6b799d53a6ba02b45c80652f4c060d80e
Markus Weinert <markusartbeat@gmail.com>
2026-08-27 17:47:52 +0200

    Aktueller Schritt -> Avatar

--- src/upgrade/CbPlanCard.vue
+++ src/upgrade/CbPlanCard.vue
@@ -16,6 +16,12 @@
       </p>
     </template>
 
+    <div v-else class="mt-6 flex items-center gap-2.5 rounded-xl bg-surface-light p-3 text-sm">
+      <!-- Free is always the plan the visitor is currently on. -->
+      <CbAvatar :name="userLabel" :image-url="userPicture" size="small" />
+      Dein aktueller Plan
+    </div>
+
     <CbDivider class="my-6" />
 
     <p class="mb-4 text-sm text-white/70">{{ benefitsTitle }}</p>
@@ -26,10 +32,19 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue'
+import CbAvatar from '../components/atoms/CbAvatar.vue'
 import CbButton from '../components/atoms/CbButton.vue'
 import CbCheckLabel from '../components/atoms/CbCheckLabel.vue'
 import CbDivider from '../components/atoms/CbDivider.vue'
 import CbIcon from '../components/atoms/CbIcon.vue'
+import { useCurrentUser } from '../composables/useCurrentUser'
+import guestPicture from '../assets/profile_pictures/profile_picture_small.png'
+
+// Signed in people see their own avatar, everyone else the guest picture.
+const { displayName, avatarUrl } = useCurrentUser()
+const userLabel = computed(() => displayName.value ?? 'Gast')
+const userPicture = computed(() => avatarUrl.value ?? guestPicture)
 
 defineProps<{
   name: string
