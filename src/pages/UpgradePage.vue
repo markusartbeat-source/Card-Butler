3e8d034c4f7a7abb236c1cd3ac5e9ddafa845bea
Markus Weinert <markusartbeat@gmail.com>
2026-08-27 17:33:13 +0200

    Glow

--- src/pages/UpgradePage.vue
+++ src/pages/UpgradePage.vue
@@ -1,6 +1,14 @@
 <template>
-  <h1 class="p-6 text-3xl text-white">Pläne, die mit dir wachsen</h1>
+  <CbGoldGlow />
 
-  <!-- The plan cards sit centered in the space below the heading. -->
-  <div class="mt-16 flex justify-center gap-8"></div>
+  <div class="relative z-10">
+    <h1 class="p-6 text-3xl text-white">Pläne, die mit dir wachsen</h1>
+
+    <!-- The plan cards sit centered in the space below the heading. -->
+    <div class="mt-16 flex justify-center gap-8"></div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import CbGoldGlow from '../upgrade/CbGoldGlow.vue'
+</script>
---
+++ src/upgrade/CbGoldGlow.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- The golden light at the bottom of the page, exported from the design. -->
+  <img
+    src="../assets/visuals/Sun.svg"
+    alt=""
+    width="1425"
+    height="497"
+    class="pointer-events-none absolute bottom-0 left-1/2 w-4/5 -translate-x-1/2"
+  />
+</template>
